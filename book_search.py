a1b6049619dffee4f318fd4e353f56247424d73c
joacopolo <jtpolonuer@gmail.com>
2021-07-24 14:03:21 -0300

    a

--- book_search.py
+++ book_search.py
@@ -1,5 +1,6 @@
 import pandas as pd
 def book_titles_from_ids(books_df, ids):
-    titles = []
-    books_df["book_id"].isin(ids)
-        
+    return list(books_df[books_df["book_id"].isin(id)]["title"])
+
+def user_ratings_by_id(ratings_df, user_id):
+    return 
